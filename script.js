1117878aff1c145c9609678f292948654a1298e8
t2wang <t2wang@ucsd.edu>
2021-04-30 16:14:54 -0700

    Part 1 of lab5

--- script.js
+++ script.js
@@ -2,14 +2,101 @@
 
 const img = new Image(); // used to load image from <input> and draw to canvas
 
+const file = document.getElementById('image-input');
+const canvas = document.getElementById('user-image');
+const ctx = canvas.getContext('2d');
+const text = document.getElementById('generate-meme');
+const clear = document.querySelector('button[type="reset"]');
+const read = document.querySelector('button[type="button"]');
+const volume = document.querySelector('input[type="range"]');
+const synth = window.speechSynthesis;
+const voice = new SpeechSynthesisUtterance();
+const icon = document.querySelector('img');
+
+const textTop = document.getElementById('text-top');
+const textBottom = document.getElementById('text-bottom');
+
+
+file.addEventListener('change', () => {
+  img.src = URL.createObjectURL(file.files[0]);
+  //files.getElementsByTagName('input')[0].src;
+
+})
+
 // Fires whenever the img object loads a new image (such as with img.src =)
 img.addEventListener('load', () => {
   // TODO
+  clear.disabled = true;
+  read.disabled = true;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, 400, 400);
+
+  var startX = canvas.width / 2;
+  var startY = canvas.height / 2;
+
+  if(img.height / img.width > 1){
+    img.height = canvas.height;
+    img.width = canvas.width * (canvas.width / img.width);
+    startX = startX - img.width / 2;
+    startY = startY - img.height / 2;
+    ctx.drawImage(img, startX, startY, img.width, img.height);
+  }else if(img.height / img.width < 1){
+    img.height = canvas.height * (canvas.height / img.height);
+    img.width = canvas.width;
+    startX = startX - img.width / 2;
+    startY = startY - img.height / 2;
+    ctx.drawImage(img, startX, startY, img.width, img.height);
+  }else{
+    img.height = canvas.height;
+    img.width = canvas.width;
+    startX = startX - img.width / 2;
+    startY = startY - img.height / 2;
+    ctx.drawImage(img, startX, startY, img.width, img.height);
+  }
 
   // Some helpful tips:
   // - Fill the whole Canvas with black first to add borders on non-square images, then draw on top
   // - Clear the form when a new image is selected
   // - If you draw the image to canvas here, it will update as soon as a new image is selected
+});
+
+text.addEventListener('submit', function(e) {
+  e.preventDefault();
+  clear.disabled = false;
+  read.disabled = false;
+  ctx.font = "25px Arial";
+  ctx.fillStyle = 'white';
+  ctx.textAlign = "center";
+  ctx.fillText(textTop.value, 200, 20);
+  ctx.fillText(textBottom.value, 200, 390);
+},false);
+
+clear.addEventListener('click', ()=> {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+read.addEventListener('click', ()=> {
+  const speech = textTop.value + " " + textBottom.value;
+  voice.text = speech;
+  synth.speak(voice);
+});
+
+volume.addEventListener('input', ()=>{
+  voice.volume = volume.value / 100;
+  if(volume.value >= 67 && volume.value <= 100){
+    icon.src = "icons/volume-level-3.svg";
+    icon.alt = "Volume Level 3";
+  }else if(volume.value >= 34 && volume.value <= 66){
+    icon.src = "icons/volume-level-2.svg";
+    icon.alt = "Volume Level 2";
+  }else if(volume.value >= 1 && volume.value <= 33){
+    icon.src = "icons/volume-level-1.svg";
+    icon.alt = "Volume Level 1";
+  }else{
+    icon.src = "icons/volume-level-0.svg";
+    icon.alt = "Volume Level 0";
+  }
 });
 
 /**
